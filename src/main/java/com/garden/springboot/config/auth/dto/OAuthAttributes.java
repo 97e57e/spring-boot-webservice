7c0d5c43ad69961b73584c8f4ac3e140641b3c26
GardenLee <khjvvv7@gmail.com>
2020-02-20 22:22:57 +0900

    Google 로그인

--- src/main/java/com/garden/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/garden/springboot/config/auth/dto/OAuthAttributes.java
@@ -30,6 +30,16 @@ public class OAuthAttributes {
         return ofGoogle(userNameAttributeName, attributes);
     }
 
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
     public User toEntity() {
         return User.builder()
                 .name(name)
